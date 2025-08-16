2781ebcf96d0a2091b9a5f5925c8f5bf0a628439
sohee <80902808+bbibbu0223@users.noreply.github.com>
2025-08-17 01:27:39 +0900

    Update include.js

--- js/include.js
+++ js/include.js
@@ -2,9 +2,9 @@ document.write(`
     <link rel="stylesheet" as="style" crossorigin
         href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard-dynamic-subset.min.css" />
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-    <link href="/css/reset.css" rel="stylesheet" />
-    <link href="/css/style.css" rel="stylesheet" />
-    <link href="/css/common.css" rel="stylesheet" />
+    <link href="./css/reset.css" rel="stylesheet" />
+    <link href="./css/style.css" rel="stylesheet" />
+    <link href="./css/common.css" rel="stylesheet" />
 
     <script src="https://kit.fontawesome.com/22ecdadb1a.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
